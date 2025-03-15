75842eed79a8ba0a70c8339c6817030e2c2da2d0
Catherine Harrison <cate.e.harrison@gmail.com>
2025-03-15 09:36:55 -0400

    animation

--- scripts/introduction.gd
+++ scripts/introduction.gd
@@ -1,6 +1,10 @@
 extends Control 
 
+var animation_scene = preload("res://scenes/orientation.tscn").instantiate()
+
+func _ready():
+	get_tree().get_root().add_child(animation_scene)
+
 func _on_play_button_pressed() -> void:
-	_ready()
-	#get_tree().change_scene("res://scenes/orientation.tscn")
-	pass # Replace with function body.
+	animation_scene.play_animation()
+	#pass # Replace with function body.
