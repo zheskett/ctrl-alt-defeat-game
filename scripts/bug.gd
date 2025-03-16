2b0861cc95509ab96eb489c2b280ddf74c0c7935
Zachary Heskett <zheskett@gmail.com>
2025-03-16 13:52:10 -0400

    bug

---
+++ scripts/bug.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.visible = false
+	bug()
+
+func bug() -> void:
+	self.show()
+	self.position.x = -900
+	self.position.y = randi_range(-200,800)
